2a2cf72c56a344d722bafd5d34d0958dbdcdf113
libyano93ly <younus93ly@gmail.com>
2022-08-04 10:26:37 +0200

    Update Simple Enum add Enum.IsDefined and Enum.tryParse

--- Enums/Program.cs
+++ Enums/Program.cs
@@ -6,11 +6,27 @@ namespace Enums
     {
         static void Main(string[] args)
         {
-            var day = (DAY.SATURDAY | DAY.SUNDAY);
-            if (day.HasFlag(DAY.WEEKEND))
+            var day = "FEB1";
+            if (Enum.TryParse(day, out Month month))
             {
-                Console.WriteLine("enjoy your weekend");
+                Console.WriteLine(month);
             }
+            else
+            {
+                Console.WriteLine("Invalid Entry");
+            }
+
+
+            var day1 = 2;
+            if (Enum.IsDefined(typeof(Month), day1))
+            {
+                Console.WriteLine(((Month) day1));
+            }
+            else
+            {
+                Console.WriteLine("Invalid Entry");
+            }
+
             Console.ReadKey();
         }
     }
@@ -33,16 +49,16 @@ namespace Enums
 
     enum DAY  //FLAGS ENUM
     {
-        NONE     = 0b_0000_0000,//0
-        MONDAY   = 0b_0000_0001,//1
+        NONE = 0b_0000_0000,//0
+        MONDAY = 0b_0000_0001,//1
         TUESDAYM = 0b_0000_0010,//2
         WHENSDAY = 0b_0000_0100,//4
         THURSDAY = 0b_0000_1000,//8
-        FRAIDAY  = 0b_0001_0000,//16
+        FRAIDAY = 0b_0001_0000,//16
         SATURDAY = 0b_0010_0000,//32
-        SUNDAY   = 0b_0100_0000,//64
-        BUSNISDAY   = MONDAY | TUESDAYM | WHENSDAY | THURSDAY | FRAIDAY ,
-        WEEKEND  = SATURDAY | SUNDAY// 0b_0010_0000 + 0b_0100_0000 = 0b_0110_0000
+        SUNDAY = 0b_0100_0000,//64
+        BUSNISDAY = MONDAY | TUESDAYM | WHENSDAY | THURSDAY | FRAIDAY,
+        WEEKEND = SATURDAY | SUNDAY// 0b_0010_0000 + 0b_0100_0000 = 0b_0110_0000
 
     }
 }
