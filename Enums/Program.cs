7e0149344219c1aaeece15da47679453f93ac093
libyano93ly <younus93ly@gmail.com>
2022-08-04 10:12:10 +0200

    FLAGS ENUM

--- Enums/Program.cs
+++ Enums/Program.cs
@@ -13,7 +13,7 @@ namespace Enums
 
     enum Month //SIMPLE ENUM
     {
-        JAN=1,
+        JAN = 1,
         FEB,
         MAR,
         APR,
@@ -25,5 +25,20 @@ namespace Enums
         OCT,
         NOV,
         DEC
+    }
+
+    enum DAY  //FLAGS ENUM
+    {
+        NONE     = 0b_0000_0000,//0
+        MONDAY   = 0b_0000_0001,//1
+        TUESDAYM = 0b_0000_0010,//2
+        WHENSDAY = 0b_0000_0100,//4
+        THURSDAY = 0b_0000_1000,//8
+        FRAIDAY  = 0b_0001_0000,//16
+        SATURDAY = 0b_0010_0000,//32
+        SUNDAY   = 0b_0100_0000,//64
+        BUSNISDAY   = MONDAY | TUESDAYM | WHENSDAY | THURSDAY | FRAIDAY ,
+        WEEKEND  = SATURDAY | SUNDAY// 0b_0010_0000 + 0b_0100_0000 = 0b_0110_0000
+
     }
 }
